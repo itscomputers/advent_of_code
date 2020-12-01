722395743ed461fbc6603f7d5e952b363c5befba
itscomputers <its_computers@protonmail.com>
2020-12-01 11:09:19 -0700

    add spec for sanitized input

--- spec/day01_spec.rb
+++ spec/day01_spec.rb
@@ -1,6 +1,16 @@
 require 'advent/day01'
 
 describe Advent::Day01 do
+  describe ".sanitized_input" do
+    subject { described_class.sanitized_input }
+
+    it { is_expected.to be_a Array }
+
+    it "has integer values" do
+      expect(subject.map(&:class).uniq).to eq [Integer]
+    end
+  end
+
   describe "example 1" do
     let(:input) { [1721, 979, 366, 299, 675, 1456] }
     let(:sum) { 2020 }
