815e6fcd829d2fa0b8bf7a4b97aeeb486500a61d
itscomputers <its_computers@protonmail.com>
2020-12-18 17:45:32 -0700

    really lean into the ruby parser order of operations

--- lib/advent/day18.rb
+++ lib/advent/day18.rb
@@ -18,83 +18,46 @@ module Advent
 
     def solve(part:)
       case part
-      when 1 then expressions.map(&:evaluate).sum
-      when 2 then @input.map { |string| string.evaluate_with_modified_order_of_operations }.sum
+      when 1 then @input.map(&method(:no_order_of_operations)).sum
+      when 2 then @input.map(&method(:opposite_order_of_operations)).sum
       end
     end
 
-    class Expression
-      attr_reader :chars
+    def no_order_of_operations(string)
+      eval(
+        string
+          .gsub("*", "-")
+          .gsub(/\d+/) { |val| "SubtractIsMultiply.new(#{val})" }
+      ).value
+    end
 
-      def initialize(chars)
-        @chars = chars
-        simplify_chars!
+    def opposite_order_of_operations(string)
+      eval(
+        string
+          .gsub(/[+*]/, "+" => "*", "*" => "+")
+          .gsub(/\d+/) { |val| "SwapAddAndMultiply.new(#{val})" }
+      ).value
+    end
+
+    class SubtractIsMultiply < Struct.new(:value)
+      def +(other)
+        self.class.new(value + other.value)
       end
 
-      def sub_expression_indeces
-        @sub_expression_indeces ||= begin
-          parentheses_count = 0
-          indeces = []
-
-          @chars.each_with_index do |char, index|
-            if char == "("
-              if parentheses_count == 0
-                @start_index = index
-              end
-              parentheses_count += 1
-            elsif char == ")"
-              if parentheses_count == 1
-                indeces << [@start_index, index]
-              end
-              parentheses_count -= 1
-            end
-          end
-
-          indeces
-        end
-      end
-
-      def simplify_chars!
-        return self if sub_expression_indeces.empty?
-
-        @index = 0
-        chars = sub_expression_indeces.reduce(Array.new) do |array, (i0, i1)|
-          [
-            *array,
-            *@chars[@index...i0],
-            self.class.new(@chars[(i0+1)...i1]).evaluate,
-          ].tap { @index = i1 + 1 }
-        end
-        @chars = [*chars, *@chars[@index..]]
-        self
-      end
-
-      def evaluate
-        value = @chars.shift
-        @chars.each_slice(2).reduce(value) do |acc, (op, val)|
-          eval [acc, op, val].join("")
-        end
+      def -(other)
+        self.class.new(value * other.value)
       end
     end
-  end
-end
 
-class ModifiedInteger < Struct.new(:value)
-  def +(other)
-    self.class.new(value * other.value)
-  end
+    class SwapAddAndMultiply < Struct.new(:value)
+      def +(other)
+        self.class.new(value * other.value)
+      end
 
-  def *(other)
-    self.class.new(value + other.value)
-  end
-end
-
-class String
-  def evaluate_with_modified_order_of_operations
-    eval(
-      gsub(/[+*]/, "+" => "*", "*" => "+")
-      .gsub(/\d+/) { |val| "ModifiedInteger.new(#{val})" }
-    ).value
+      def *(other)
+        self.class.new(value + other.value)
+      end
+    end
   end
 end
 
