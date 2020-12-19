5da036271ec402f25e7f2192ac5d4669a6c1e72e
itscomputers <its_computers@protonmail.com>
2020-12-18 17:50:10 -0700

    fix specs after refactor

--- spec/day18_spec.rb
+++ spec/day18_spec.rb
@@ -1,72 +1,44 @@
 require 'advent/day18'
 
 describe Advent::Day18 do
-  describe Advent::Day18::Expression do
-    let(:expression) { described_class.new string.gsub(/ +/, "").chars }
+  let(:day) { described_class.build }
 
-    describe "#chars" do
-      subject { expression.chars }
+  describe "#no_order_of_operations" do
+    subject { day.no_order_of_operations(string) }
 
-      before { allow_any_instance_of(described_class).to receive(:evaluate).and_return('hi') }
-
-      context "when string is 1 + 2 * 3 + 4 * 5 + 6" do
-        let(:string) { "1 + 2 * 3 + 4 * 5 + 6" }
-        it { is_expected.to eq string.split(" ") }
-      end
-
-      context "when string is 2 * 3 + (4 * 5)" do
-        let(:string) { "2 * 3 + (4 * 5)" }
-        it { is_expected.to eq "2 * 3 + hi".split(" ") }
-      end
-
-      context "when string is 1 + (2 * 3) + (4 * (5 + 6))" do
-        let(:string) { "1 + (2 * 3) + (4 * (5 + 6))" }
-        it { is_expected.to eq "1 + hi + hi".split(" ") }
-      end
-
-      context "when string is ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" do
-        let(:string) { "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" }
-        it { is_expected.to eq "hi + 2 + 4 * 2".split(" ") }
-      end
+    context "when string is 1 + 2 * 3 + 4 * 5 + 6" do
+      let(:string) { "1 + 2 * 3 + 4 * 5 + 6" }
+      it { is_expected.to eq 71 }
     end
 
-    describe "#evaluate" do
-      subject { expression.evaluate }
+    context "when string is 1 + (2 * 3) + (4 * (5 + 6))" do
+      let(:string) { "1 + (2 * 3) + (4 * (5 + 6))" }
+      it { is_expected.to eq 51 }
+    end
 
-      context "when string is 1 + 2 * 3 + 4 * 5 + 6" do
-        let(:string) { "1 + 2 * 3 + 4 * 5 + 6" }
-        it { is_expected.to eq 71 }
-      end
+    context "when string is 2 * 3 + (4 * 5)" do
+      let(:string) { "2 * 3 + (4 * 5)" }
+      it { is_expected.to eq 26 }
+    end
 
-      context "when string is 1 + (2 * 3) + (4 * (5 + 6))" do
-        let(:string) { "1 + (2 * 3) + (4 * (5 + 6))" }
-        it { is_expected.to eq 51 }
-      end
+    context "when string is 5 + (8 * 3 + 9 + 3 * 4 * 3)" do
+      let(:string) { "5 + (8 * 3 + 9 + 3 * 4 * 3)" }
+      it { is_expected.to eq 437 }
+    end
 
-      context "when string is 2 * 3 + (4 * 5)" do
-        let(:string) { "2 * 3 + (4 * 5)" }
-        it { is_expected.to eq 26 }
-      end
+    context "when string is 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" do
+      let(:string) { "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" }
+      it { is_expected.to eq 12240 }
+    end
 
-      context "when string is 5 + (8 * 3 + 9 + 3 * 4 * 3)" do
-        let(:string) { "5 + (8 * 3 + 9 + 3 * 4 * 3)" }
-        it { is_expected.to eq 437 }
-      end
-
-      context "when string is 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" do
-        let(:string) { "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" }
-        it { is_expected.to eq 12240 }
-      end
-
-      context "when string is ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" do
-        let(:string) { "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" }
-        it { is_expected.to eq 13632 }
-      end
+    context "when string is ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" do
+      let(:string) { "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" }
+      it { is_expected.to eq 13632 }
     end
   end
 
-  describe "modified order of operations" do
-    subject { string.evaluate_with_modified_order_of_operations }
+  describe "#opposite_order_of_operations" do
+    subject { day.opposite_order_of_operations(string) }
 
     context "when string is 1 + 2 * 3 + 4 * 5 + 6" do
       let(:string) { "1 + 2 * 3 + 4 * 5 + 6" }
