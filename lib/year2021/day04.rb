4e6c2ecbde073ab82d12335d5064f6e39c9bd543
itscomputers <its_computers@protonmail.com>
2021-12-04 22:23:42 -0700

    day 04

--- lib/year2021/day04.rb
+++ lib/year2021/day04.rb
@@ -4,46 +4,88 @@ module Year2021
   class Day04 < Solver
     def solve(part:)
       case part
-      when 1 then play_all
+      when 1 then Bingo.new(numbers, boards).play_first.winning_score
+      when 2 then Bingo.new(numbers, boards).play_last.winning_score
       end
     end
 
     def numbers
-      @numbers ||= lines.first
+      @numbers ||= lines.first.split(",")
     end
 
     def boards
       @boards ||= lines.drop(1).each_slice(6).map { |slice| Board.new(slice.drop(1)) }
     end
 
-    def play_all
-      index = 0
-      while boards.none?(:complete?)
-        boards.each { |board| board.call(numbers[index]) }
+    class Bingo
+      attr_reader :numbers, :boards
+
+      def initialize(numbers, boards)
+        @numbers = numbers
+        @boards = boards
       end
-      board.select(&:complete?).first.score
+
+      def inspect
+        "<Bingo boards: #{@boards.map { |board| [*board.rows.map(&:size), *board.cols.map(&:size)].max }.join(", ") }>"
+      end
+
+      def play_turn
+        number = @numbers.shift
+        @boards.each do |board|
+          board.call(number)
+        end
+      end
+
+      def play_first
+        play_turn until @boards.any?(&:complete?)
+        @winning_board = @boards.select(&:complete?).first
+        self
+      end
+
+      def play_last
+        play_first
+        play_turn until @boards.reject(&:complete?).size == 1
+        @winning_board = @boards.reject(&:complete?).first
+        play_turn until @winning_board.complete?
+        self
+      end
+
+      def winning_score
+        @winning_board.score
+      end
     end
 
     class Board
+      attr_reader :rows, :cols
+
       def initialize(lines)
-        @values = lines.flat_map { |line| line.replace("  ", " ").split(" ") }
+        @values = lines.flat_map { |line| line.gsub("  ", " ").split(" ") }
         @rows = [[], [], [], [], []]
         @cols = [[], [], [], [], []]
       end
 
+      def inspect
+        @values.each_slice(5).map { |row| row.join(" ") }.join("\n")
+      end
+
       def call(number)
-        row, col = @values.index(number).divmod(5)
+        return if complete?
+
+        index = @values.index(number)
+        return if index.nil?
+
+        row, col = index.divmod(5)
         @rows[row] << number
         @cols[col] << number
         @values[index] = nil
       end
 
       def complete?
-        [*@rows, *@cols].map(&:size).max == 5
+        @complete ||= ([*@rows, *@cols].map(&:size).max == 5)
       end
 
       def score
-        @values.compact.sum * [*@row, *@cols].max_by(&:count).last
+        @values.compact.map(&:to_i).sum * [*@rows, *@cols].max_by(&:size).last.to_i
       end
     end
   end
