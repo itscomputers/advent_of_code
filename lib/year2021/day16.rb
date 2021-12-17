46598e9c1304b3b84257e43ee30e87339e0a9a1f
itscomputers <its_computers@protonmail.com>
2021-12-16 21:40:03 -0700

    refactor to get part 2 to pass

--- lib/year2021/day16.rb
+++ lib/year2021/day16.rb
@@ -4,16 +4,208 @@ module Year2021
   class Day16 < Solver
     def solve(part:)
       case part
-      when 1 then outer_packet.version_sum
-      when 2 then outer_packet.value
+      when 1 then packet.version_sum
+      when 2 then packet.value
       end
     end
 
     def outer_packet
-      @outer_packet ||= Packet.for_hex(lines.first).process_subpackets
+      @outer_packet ||= OldPacket.for_hex(lines.first).process_subpackets
+    end
+
+    def packet
+      @packet ||= Packet.new(BitStream.new(lines.first, type: :hex))
+    end
+
+    class BitStream
+      HEX_MAP = {
+        "0" => "0000",
+        "1" => "0001",
+        "2" => "0010",
+        "3" => "0011",
+        "4" => "0100",
+        "5" => "0101",
+        "6" => "0110",
+        "7" => "0111",
+        "8" => "1000",
+        "9" => "1001",
+        "A" => "1010",
+        "B" => "1011",
+        "C" => "1100",
+        "D" => "1101",
+        "E" => "1110",
+        "F" => "1111",
+      }
+
+      def initialize(input, type: :binary)
+        if type == :hex
+          @stream = input.each_char.lazy.flat_map { |char| HEX_MAP[char].each_char.lazy }
+        else
+          @stream = input.each_char.lazy
+        end
+      end
+
+      def take(bits=1)
+        bits.times.map { @stream.next }.join
+      end
+
+      def peek
+        @stream.peek
+      end
+
+      def read_int(bits=1)
+        take(bits).to_i(2)
+      end
     end
 
     class Packet
+      attr_reader :bit_stream, :version, :type_id, :subpackets
+
+      def initialize(bit_stream)
+        @bit_stream = bit_stream
+        @version = @bit_stream.read_int(3)
+        @type_id = @bit_stream.read_int(3)
+        @subpackets = []
+        parse
+      rescue StopIteration
+        @version = -1
+        @type_id = -1
+        @subpackets = []
+        @value = 0
+      end
+
+      def parse
+        if @type_id == 4
+          parse_literal
+        else
+          parse_length_type
+        end
+      end
+
+      def parse_literal
+        @value = LiteralPacketParser.new(@bit_stream).value
+      end
+
+      def parse_length_type
+        length_type_id = @bit_stream.read_int
+        @length = @bit_stream.read_int(15 - 4 * length_type_id)
+        if length_type_id == 0
+          parse_length_type_0
+        else
+          parse_length_type_1
+        end
+        self
+      end
+
+      def parse_length_type_0
+        local_bit_stream = BitStream.new(@bit_stream.take(@length))
+        loop do
+          local_bit_stream.peek
+          Packet.new(local_bit_stream).tap do |packet|
+            @subpackets << packet if packet.valid?
+          end
+        rescue StopIteration
+          break
+        end
+        self
+      end
+
+      def parse_length_type_1
+        until @subpackets.size == @length
+          Packet.new(@bit_stream).tap do |packet|
+            @subpackets << packet if packet.valid?
+          end
+        end
+        self
+      end
+
+      def valid?
+        @version > -1
+      end
+
+      def version_sum
+        @version + @subpackets.sum(&:version_sum)
+      end
+
+      def value
+        @value ||= case @type_id
+        when 0 then @subpackets.sum(&:value)
+        when 1 then @subpackets.map(&:value).reduce(1) { |acc, value| acc * value }
+        when 2 then @subpackets.min_by(&:value).value
+        when 3 then @subpackets.max_by(&:value).value
+        when 5 then binary_operator(:>)
+        when 6 then binary_operator(:<)
+        when 7 then binary_operator(:==)
+        end
+      end
+
+      def binary_operator(operator)
+        @subpackets.first.value.send(operator, @subpackets.last.value) ? 1 : 0
+      end
+    end
+
+    class LiteralPacketParser
+      def initialize(bit_stream)
+        @bit_stream = bit_stream
+      end
+
+      def value
+        return @value unless @value.nil?
+        values = []
+        values << @bit_stream.take(4) while @bit_stream.read_int == 1
+        values << @bit_stream.take(4)
+        @value = values.join.to_i(2)
+      end
+    end
+
+    class OperatorPacketParser
+      def initialize(bit_stream)
+        bit_stream = bit_stream
+        length_type_id = bit_stream.read_int
+        length = bit_stream.read_int(15 - 4 * length_type_id)
+        sub_class = length_type_id == 0 ? Type0Parser : Type1Parser
+        @sub_parser = sub_class.new(bit_stream, length)
+      end
+
+      def subpackets
+        @sub_parser.subpackets
+      end
+
+      class Type0Parser
+        def initialize(bit_stream, length)
+          @bit_stream = BitStream.new(bit_stream.take(length))
+        end
+
+        def subpackets
+          return @subpackets unless @subpackets.nil?
+          @subpackets = []
+          loop do
+            @bit_stream.peek
+            Packet.new(@bit_stream).tap do |packet|
+              @subpackets << packet if packet.valid?
+            end
+          rescue StopIteration
+            break
+          end
+        end
+      end
+
+      class Type1Parser
+        def initialize(bit_stream, length)
+          @bit_stream = bit_stream
+          @length = length
+        end
+
+        def subpackets
+          @subpackets ||= @length.times.map do
+            Packet.new(@bit_stream)
+          end
+        end
+      end
+    end
+
+
+    class OldPacket
       attr_reader :binary, :version, :type_id, :subpackets
 
       HEX_MAP = {
@@ -69,7 +261,7 @@ module Year2021
       end
     end
 
-    class EmptyPacket < Packet
+    class EmptyPacket < OldPacket
       def initialize(binary)
         @binary = binary
         @version = 0
@@ -85,7 +277,7 @@ module Year2021
       end
     end
 
-    class LiteralPacket < Packet
+    class LiteralPacket < OldPacket
       def initialize(binary, version, type_id)
         @binary = binary
         @version = version
@@ -118,7 +310,7 @@ module Year2021
       end
     end
 
-    class OperatorPacket < Packet
+    class OperatorPacket < OldPacket
       def initialize(binary, version, type_id)
         @binary = binary
         @version = version
@@ -159,7 +351,7 @@ module Year2021
       end
 
       def subpacket
-        Packet.for_binary(leftover)
+        OldPacket.for_binary(leftover)
       end
 
       def process_subpacket
--- spec/year2021/day16_spec.rb
+++ spec/year2021/day16_spec.rb
@@ -178,18 +178,10 @@ describe Year2021::Day16 do
   end
 
   describe "packets" do
-    let(:packet) { Year2021::Day16::Packet.for_hex(hex) }
+    let(:packet) { Year2021::Day16::Packet.new(Year2021::Day16::BitStream.new(hex, type: :hex)) }
 
     describe "literal packet" do
       let(:hex) { "D2FE28" }
-
-      it "is a literal packet" do
-        expect(packet).to be_a Year2021::Day16::LiteralPacket
-      end
-
-      it "has binary 110100101111111000101000" do
-        expect(packet.binary).to eq "110100101111111000101000"
-      end
 
       it "has version 6" do
         expect(packet.version).to eq 6
@@ -203,25 +195,14 @@ describe Year2021::Day16 do
         expect(packet.value).to eq 2021
       end
 
-      it "has groups 10111 11110 00101" do
-        expect(packet.groups).to eq %w(10111 11110 00101)
-      end
-
       it "has leftover 000" do
-        expect(packet.leftover).to eq "000"
+        expect(packet.bit_stream.take(3)).to eq "000"
+        expect { packet.bit_stream.take }.to raise_exception StopIteration
       end
     end
 
     describe "operator packet with length type id 0" do
       let(:hex) { "38006F45291200" }
-
-      it "is an operator packet" do
-        expect(packet).to be_a Year2021::Day16::OperatorPacket
-      end
-
-      it "has binary 00111000000000000110111101000101001010010001001000000000" do
-        expect(packet.binary).to eq "00111000000000000110111101000101001010010001001000000000"
-      end
 
       it "has version 1" do
         expect(packet.version).to eq 1
@@ -230,59 +211,25 @@ describe Year2021::Day16 do
       it "has type_id 6" do
         expect(packet.type_id).to eq 6
       end
-      it "has length type id 0" do
-        expect(packet.length_type_id).to eq 0
-      end
-
-      it "has bit field length 15" do
-        expect(packet.bit_field_length).to eq 15
-      end
-
-      it "has bit field 000000000011011" do
-        expect(packet.bit_field).to eq "000000000011011"
-      end
-
-      it "has length 27" do
-        expect(packet.length).to eq 27
-      end
-
-      it "has leftover 1101000101001010010001001000000000" do
-        expect(packet.leftover).to eq "1101000101001010010001001000000000"
-      end
-
-      it "has a first subpacket with value 10 and leftover 01010010001001000000000" do
-        packet.subpacket.tap do |subpacket|
-          expect(subpacket.value).to eq 10
-          expect(subpacket.leftover).to eq "01010010001001000000000"
-        end
-      end
-
-      it "after processing first subpacket, it has a leftover of 01010010001001000000000" do
-        packet.process_subpacket
-        expect(packet.leftover).to eq "01010010001001000000000"
-      end
-
-      it "has a second subpacket with value 20 and leftover 0000000" do
-        packet.process_subpacket.subpacket.tap do |subpacket|
-          expect(subpacket.value).to eq 20
-          expect(subpacket.leftover).to eq "0000000"
-        end
-      end
 
       it "has a total of two subpackets with values of 10 and 20" do
-        expect(packet.process_subpackets.subpackets.map(&:value)).to eq [10, 20]
+        expect(packet.subpackets.map(&:value)).to eq [10, 20]
       end
     end
 
-    describe "operator packet with length type id 0" do
+    describe "operator packet with length type id 1" do
       let(:hex) { "EE00D40C823060" }
 
-      it "is an operator packet" do
-        expect(packet).to be_a Year2021::Day16::OperatorPacket
+      it "has version 7" do
+        expect(packet.version).to eq 7
+      end
+
+      it "has type_id 3" do
+        expect(packet.type_id).to eq 3
       end
 
       it "has 3 subpackets with values 1, 2, 3" do
-        expect(packet.process_subpackets.subpackets.map(&:value)).to eq [1, 2, 3]
+        expect(packet.subpackets.map(&:value)).to eq [1, 2, 3]
       end
     end
   end
