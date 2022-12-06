68f878b1af0bee7702ce1650c29259e04051ea1b
itscomputers <its_computers@protonmail.com>
2022-12-05 22:13:45 -0700

    refactor and write specs

--- lib/graph.rb
+++ lib/graph.rb
@@ -11,17 +11,20 @@ class Graph
     @node_lookup.values
   end
 
-  def node_for(value)
-    @node_lookup[value] ||= self.class::Node.new value
+  def node(value)
+    @node_lookup[value] ||= self.class::Node.new(value)
   end
 
-  def neighbors_of(node)
-    node.neighbors
+  def neighbors(value)
+    node(value).neighbors
   end
 
-  def distance(node, other)
-    raise unless neighbors_of(node).include? other
-    node.weight_for other
+  def distance(value, other)
+    node(value).weight(other)
+  end
+
+  def adjacent?(value, other)
+    node(value).has_neighbor?(other)
   end
 
   class Node < Struct.new(:value)
@@ -29,44 +32,87 @@ class Graph
       "<Graph::Node #{value}>"
     end
 
+    def edges
+      @edges ||= Hash.new
+    end
+
+    def add_edge(node, weight)
+      edges[node.value] = Edge.new(self, node, weight)
+    end
+
     def neighbors
-      @neighbors ||= Hash.new
+      edges.values.map(&:target)
     end
 
-    def add_neighbor(node, weight)
-      neighbors[node] = weight
+    def has_neighbor?(value)
+      edges.key?(value)
     end
 
-    def has_neighbor?(node)
-      neighbors.key? node
-    end
-
-    def weight_for(node)
-      raise unless has_neighbor? node
-      neighbors[node]
+    def weight(value)
+      edges[value]&.weight
     end
   end
 
-  class Builder
+  class Edge < Struct.new(:source, :target, :weight)
+    def inspect
+      "<Graph::Edge #{source.value} --#{weight}--> #{target.value}>"
+    end
+  end
+
+  class DirectedGraphBuilder
     def initialize(hash)
       @hash = hash
       @graph = Graph.new
     end
 
     def build
-      @hash.each do |value, neighbor_values|
-        node = @graph.node_for(value)
-        if neighbor_values.is_a?(Hash)
-          neighbor_values.each do |neighbor_value, weight|
-            node.add_neighbor(@graph.node_for(neighbor_value), weight)
-          end
-        else
-          neighbor_values.each do |neighbor_value|
-            node.add_neighbor(@graph.node_for(neighbor_value), 1)
-          end
+      @hash.keys.each do |key|
+        node = @graph.node(key)
+        neighbor_values(node).each do |neighbor_value|
+          neighbor = @graph.node(neighbor_value)
+          add_edges(node, neighbor)
         end
       end
       @graph
+    end
+
+    def neighbor_values(node)
+      @hash.dig(node.value).keys
+    end
+
+    def weight(node, neighbor)
+      @hash.dig(node.value)&.dig(neighbor.value)
+    end
+
+    def add_edges(node, neighbor)
+      node.add_edge(neighbor, weight(node, neighbor))
+    end
+  end
+
+  class UndirectedGraphBuilder < DirectedGraphBuilder
+    def add_edges(node, neighbor)
+      super
+      neighbor.add_edge(node, weight(node, neighbor))
+    end
+  end
+
+  class SimpleDirectedGraphBuilder < DirectedGraphBuilder
+    def neighbor_values(node)
+      @hash.dig(node.value)
+    end
+
+    def weight(node, neighbor)
+      1
+    end
+  end
+
+  class SimpleUndirectedGraphBuilder < UndirectedGraphBuilder
+    def neighbor_values(node)
+      @hash.dig(node.value)
+    end
+
+    def weight(node, neighbor)
+      1
     end
   end
 end
