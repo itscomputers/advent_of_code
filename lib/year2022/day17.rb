ba67dd2d2dd5f728abad598851e8c67d3639af0c
itscomputers <its_computers@protonmail.com>
2022-12-17 22:15:28 -0700

    solve 2022 day 17

--- lib/year2022/day17.rb
+++ lib/year2022/day17.rb
@@ -6,13 +6,9 @@ require "vector"
 module Year2022
   class Day17 < Solver
     def solve(part:)
-      tetris.handle_pieces(count: piece_count(part: part)).height
-    end
-
-    def piece_count(part:)
       case part
-      when 1 then 2022
-      when 2 then 1000000000000
+      when 1 then height_after(count: 2022)
+      when 2 then height_after(count: 1000000000000)
       end
     end
 
@@ -31,25 +27,79 @@ module Year2022
     end
 
     def tetris
-      @tetris ||= Tetris.new(shapes, gusts)
+      Tetris.new(shapes, gusts)
+    end
+
+    def offset
+      38 * 5
+    end
+
+    def period
+      345 * 5
+    end
+
+    def height_after(count:)
+      HeightCalculator.new(tetris, offset, period, count).height
+    end
+
+    class HeightCalculator
+      def initialize(tetris, offset, period, count)
+        @tetris = tetris
+        @offset = offset
+        @period = period
+        @count = count
+      end
+
+      def heights
+        @heights ||= (0..@offset + @period).map do
+          @tetris.handle_piece.height
+        end
+      end
+
+      def divmod
+        @divmod ||= (@count - @offset).divmod(@period)
+      end
+
+      def quotient
+        divmod.first
+      end
+
+      def remainder
+        divmod.last
+      end
+
+      def height
+        puts "height at #{@offset}: #{heights[@offset]}"
+        puts "height at #{@offset + remainder}: #{heights[@offset + remainder]}"
+        puts "height at #{@offset + @period}: #{heights[@offset + @period]}"
+        quotient * (heights[@offset + @period] - heights[@offset]) + heights[@offset + remainder]
+      end
     end
 
     class Tetris
-      def initialize(shapes, gusts, debug: false)
+      attr_reader :piece_count
+
+      def initialize(shapes, gusts, window_size: nil, debug: false)
         @shapes = shapes
         @gusts = gusts
+        @window_size = window_size
         @debug = debug
 
         @piece_count = 0
-        @heights = 7.times.map { [0] }
+        @points_by_y_value = {0 => (0..6).to_a}
+
+        @piece_loc = nil
         @piece = nil
+        @movements_by_shape = Hash.new
+
         @stationary = true
         @falling = false
         @gust = nil
+        @lower = 0
       end
 
       def inspect
-        "<Tetris pieces=#{@piece_count}, heights=#{@heights}, stationary=#{@stationary}>"
+        "<Tetris pieces=#{@piece_count}, height=#{height}, stationary=#{@stationary}>"
       end
       alias_method :to_s, :inspect
 
@@ -57,37 +107,64 @@ module Year2022
         return self if @piece.nil? || !@debug
         puts Grid.display(
           {
-            **([-height, @piece.y_min].min - 2..0).flat_map { |y_value| [[[-1, y_value], "|"], [[7, y_value], "|"]] }.to_h,
-            **(-1..7).map { |x_value| [[x_value, 0], "-"] }.to_h,
+            **([-height, @piece.y_min].min - 2..@lower).flat_map { |y_value| [[[-1, y_value], "|"], [[7, y_value], "|"]] }.to_h,
+            **(-1..7).map { |x_value| [[x_value, @lower], "-"] }.to_h,
             **points.map { |point| [point, "#"] }.to_h,
             **@piece.points.map { |point| [point, "@"] }.to_h,
-            [3, 1] => @gust || (@falling ? "v" : "-"),
+            [3, @lower + 1] => @gust || (@falling ? "v" : "-"),
           },
           type: :hash,
         )
         self
       end
 
+      def movements
+        @movements_by_shape
+      end
+
+      def y_range
+        @points_by_y_value.keys.minmax
+      end
+
       def height
-        @heights.map(&:min).min.abs
+        signed_height.abs
+      end
+
+      def signed_height
+        @points_by_y_value.keys.min
       end
 
       def add_heights(piece)
-        piece.border.each do |(x, y)|
-          @heights[x] = [*@heights[x], y].min(4)
+        piece.points.each do |(x, y)|
+          @points_by_y_value[y] ||= []
+          @points_by_y_value[y] << x
+        end
+        reduce!
+      end
+
+      def reduce!
+        return if @window_size.nil?
+        return unless signed_height + @window_size < @lower
+        @lower = signed_height + @window_size
+        @points_by_y_value.keys.each do |y|
+          @points_by_y_value.delete(y) if y > @lower
         end
       end
 
       def points
-        (0..6).flat_map { |x| @heights[x].map { |y| [x, y] } }
+        @points_by_y_value.flat_map { |y, x_values| x_values.map { |x| [x, y] } }
       end
 
       def point?(point)
-        @heights[point.first].include?(point.last)
+        return false unless @points_by_y_value.key?(point.last)
+        @points_by_y_value[point.last].include?(point.first)
       end
 
       def handle_pieces(count:)
-        handle_piece until @piece_count == count
+        until @piece_count == count
+          handle_piece
+          puts "height at piece ##{@piece_count}: #{height}" if @piece_count % 100 == 0
+        end
         self
       end
 
@@ -112,6 +189,7 @@ module Year2022
           piece.move_by([2, -(height + 4)])
           @stationary = false
           @gust = @gusts.next
+          @piece_loc = piece.loc
         end
         self
       end
@@ -137,6 +215,8 @@ module Year2022
           @piece.move_by([0, -1])
           @stationary = true
           @piece_count += 1
+          @movements_by_shape[@piece.shape] ||= []
+          @movements_by_shape[@piece.shape] << Vector.subtract(@piece.loc, @piece_loc)
           add_heights(@piece)
         else
           @gust = @gusts.next
@@ -152,16 +232,15 @@ module Year2022
       end
 
       class Piece
-        attr_reader :points
+        attr_reader :points, :shape
 
         def initialize(points)
           @points = points
+          @shape = points.hash
         end
 
-        def border
-          (0..6).map do |x|
-            @points.select { |point| point.first == x }.min_by(&:last)
-          end.compact
+        def loc
+          [x_min, y_min]
         end
 
         def x_min
