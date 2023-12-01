0402ec8c9dfb0fa36e2d3c3866a5c530f6e78214
dw <itscomputers@proton.me>
2023-11-30 22:48:57 -0700

    solve 2023 day 01

---
+++ lib/year2023/day01.rb
@@ -0,0 +1,37 @@
+require "solver"
+
+module Year2023
+  class Day01 < Solver
+    def solve(part:)
+      lines.map { |line| extractor(part: part).extract(line) }.sum
+    end
+
+    def extractor(part:)
+      part == 1 ? NumberExtractor : RealNumberExtractor
+    end
+
+    class NumberExtractor
+      PATTERN = /\d/
+
+      def self.extract(line)
+        [/(#{self::PATTERN}).*$/, /^.*(#{self::PATTERN})/]
+          .map { |regex| convert(regex.match(line)[1]) }
+          .join
+          .to_i
+      end
+
+      def self.convert(digit)
+        digit
+      end
+    end
+
+    class RealNumberExtractor < NumberExtractor
+      DIGITS = %w(one two three four five six seven eight nine).map.with_index.to_h
+      PATTERN = /\d|#{DIGITS.keys.join("|")}/
+
+      def self.convert(digit)
+        DIGITS.key?(digit) ? DIGITS[digit] + 1 : digit
+      end
+    end
+  end
+end
