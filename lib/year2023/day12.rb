0ec39349795142def1d78f6e276eaccfec8b3daf
dw <itscomputers@proton.me>
2023-12-14 15:12:05 -0700

    solve 2023 day 12

--- lib/year2023/day12.rb
+++ lib/year2023/day12.rb
@@ -2,158 +2,99 @@ require "solver"
 
 module Year2023
   class Day12 < Solver
+    COUNTS_BY_STATE = Hash.new
+
     def solve(part:)
       case part
-      when 1 then resolvers.map(&:resolve).map(&:count).sum
-      when 2 then unfolded_resolvers.map(&:resolve).map(&:count).sum
+      when 1 then arrangements.map(&:count).sum
+      when 2 then unfolded_arrangements.map(&:count).sum
       else nil
       end
     end
 
-    def resolvers
-      lines.map do |line|
-        Resolver.new(Arrangement.build(string(line), sizes(line)))
-      end
-    end
-
-    def unfolded_resolvers
-      lines.map do |line|
-        Resolver.new(Arrangement.unfold(string(line), sizes(line)))
-      end
-    end
-
-    def string(line)
+    def get_springs(line)
       line.split(" ").first
     end
 
-    def sizes(line)
+    def get_sizes(line)
       line.split(" ").last.split(",").map(&:to_i)
     end
 
-
-    class Resolver
-      def initialize(arrangement)
-        @terminal = []
-        @queue = [arrangement.advance]
-      end
-
-      def resolve
-        process_next until @queue.empty?
-        self
-      end
-
-      def process_next
-        arrangement = @queue.shift
-        return self if arrangement.nil?
-        return self if arrangement.invalid?
-        @terminal << arrangement if arrangement.terminal?
-        arrangement.split.each do |split|
-          next if split.invalid?
-          if split.terminal?
-            @terminal << split
-          else
-            @queue << split
-          end
-        end
-        self
-      end
-
-      def count
-        @terminal.size
-      end
+    def arrangements
+      lines.map { |line| Arrangement.build(get_springs(line), get_sizes(line)) }
     end
 
-    class Arrangement < Struct.new(:string, :sizes, :index)
-      def self.build(string, sizes)
-        new(string, sizes, 0)
+    def unfolded_arrangements
+      lines.map { |line| Arrangement.unfold(get_springs(line), get_sizes(line)) }
+    end
+
+    class Arrangement < Struct.new(:springs, :sizes, :damaged_count)
+      def self.build(springs, sizes)
+        new(springs, sizes, 0)
       end
 
-      def self.unfold(string, sizes)
+      def self.unfold(springs, sizes)
         new(
-          5.times.map { string }.join("?"),
+          5.times.map { springs }.join("?"),
           5.times.flat_map { sizes },
           0,
         )
       end
 
-      def inspect
-        "<#{string}, #{sizes}, #{index}>"
-      end
-      alias_method :to_s, :inspect
-
-      def terminal?
-        sizes.empty? && slice_could_be_empty?
+      def count
+        return get_count if count_cached?
+        springs.empty? ? set_terminal_count : set_non_terminal_count
+        get_count
       end
 
-      def invalid?
-        @invalid ||
-          (sizes.empty? && !slice_could_be_empty?) ||
-          (!sizes.empty? && slice_is_empty?)
-      end
-
-      def can_split?
-        char == "?"
-      end
-
-      def split
-        %w(# .).map do |ch|
-          Arrangement.new(string_with(ch), sizes, index).advance
-        end.reject(&:invalid?)
-      end
-
-      def can_advance?
-        !terminal? && !invalid? && !can_split?
-      end
-
-      def advance
-        while can_advance?
-          if char == "."
-            self.index += slice.index(/[?#]/)
-          else
-            if prefix.chars.any? { |ch| ch == "." }
-              @invalid = true
-            elsif prefix.length < size
-              @invalid = true
-            elsif slice.length > size && slice[size] == "#"
-              @invalid = true
-            elsif slice.length == size
-              self.index = string.length
-              self.sizes = self.sizes.drop(1)
-            else
-              self.index += size + 1
-              self.sizes = self.sizes.drop(1)
-            end
-          end
+      def set_terminal_count
+        if damaged_count > 0
+          set_count(sizes.size == 1 && end_damaged? ? 1 : 0)
+        else
+          set_count(sizes.empty? ? 1 : 0)
         end
-        self
       end
 
-      def char
-        slice.chr
+      def set_non_terminal_count
+        set_count(chars.map(&method(:next_count)).sum)
       end
 
-      def slice
-        index > string.length ? "" : string[index..]
+      def next_count(char)
+        char == "#" ? next_count_damaged : next_count_operational
       end
 
-      def prefix
-        slice[...size] || ""
+      def next_count_damaged
+        Arrangement.new(springs[1..], sizes, damaged_count + 1).count
       end
 
-      def size
-        sizes.first || 0
+      def next_count_operational
+        if damaged_count == 0
+          Arrangement.new(springs[1..], sizes, damaged_count).count
+        elsif end_damaged?
+          Arrangement.new(springs[1..], sizes.drop(1), 0).count
+        else
+          0
+        end
       end
 
-      def slice_could_be_empty?
-        slice.empty? || slice.chars.none? { |ch| ch == "#" }
+      def end_damaged?
+        sizes.first == damaged_count
       end
 
-      def slice_is_empty?
-        slice.empty? || slice.chars.all? { |ch| ch == "." }
+      def chars
+        springs.chr == "?" ? %w(# .) : [springs.chr]
       end
 
-      def string_with(ch)
-        (string[...index] || "") + ch + (slice[1..] || "")
+      def count_cached?
+        COUNTS_BY_STATE.key?(self)
+      end
+
+      def get_count
+        COUNTS_BY_STATE[self]
+      end
+
+      def set_count(count)
+        COUNTS_BY_STATE[self] = count
       end
     end
   end
